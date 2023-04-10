81f6c74c9176263ff8911465180e3e7a5b983bc8
Yahia Refaiea <YahiaRefaiea@gmail.com>
2023-04-10 13:04:54 +0800

    move routes outside app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-
-Vue.use(VueRouter)
+import router from './router'
 
 new Vue({
+  router,
 	el: '#app',
 	render: h => h(App)
 });
---
+++ src/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from './views/Home.vue'
+import Category from './views/Category.vue'
+
+Vue.use(VueRouter)
+
+const baseUrl = '/docs'
+const routes = [
+  {
+    path: `${baseUrl}/`,
+    name: 'home',
+    component: Home
+  }, {
+    path: `${baseUrl}/:id`,
+    name: 'category',
+    component: Category
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
