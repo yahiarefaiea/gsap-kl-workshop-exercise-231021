010355bf8ef8b80bb36c073392271b7bc29af6cc
Yahia Refaiea <YahiaRefaiea@gmail.com>
2023-04-11 16:16:40 +0800

    match sentences with original design

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
 <div id="header-wrapper">
   <div id="header-text">
-    <h1>Knowledge base doesn't have to be boring</h1>
-    <h3>Everything you need to manage your messaging</h3>
+    <h1>Knowledge base doesn’t have to be boring</h1>
+    <h3>Everything you need to Manage your Messaging</h3>
   </div>
 </div>
 </template>
