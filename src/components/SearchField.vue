44d69e88f4f35935bb75b0aee7dc3b8d5cc9d441
Yahia Refaiea <YahiaRefaiea@gmail.com>
2023-04-11 16:23:51 +0800

    add searchField

---
+++ src/components/SearchField.vue
@@ -0,0 +1,57 @@
+<template>
+<div id="search-field">
+  <tawk-field
+    :placeholder="'Search for answers'"
+    :value="query"
+    @update:value="captureValue"
+    :style="{
+      borderTopRightRadius: 0,
+      borderBottomRightRadius: 0,
+      borderRight: 0
+    }"
+  />
+  <tawk-button
+    :action="submitSearchQuery"
+    :isIcon="true"
+    :cta="'search'"
+    :style="{
+      borderTopLeftRadius: 0,
+      borderBottomLeftRadius: 0,
+      flex: '80px',
+      flexShrink: 0
+    }"
+  />
+</div>
+</template>
+
+<script>
+import Field from './Field.vue'
+import Button from './Button.vue'
+
+export default {
+  components: {
+    'tawk-field': Field,
+    'tawk-button': Button
+  },
+  data: () => ({
+    query: ''
+  }),
+  methods: {
+    captureValue(value) {
+      this.query = value
+      console.log(this.query)
+    },
+    submitSearchQuery() {
+      console.log('submitting..')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#search-field {
+  display: flex;
+  max-width: 720px;
+  margin: 0 auto;
+}
+</style>
