7e066b86389136485bb557e7202e8b1da94db2cd
Yahia Refaiea <YahiaRefaiea@gmail.com>
2023-04-13 09:48:00 +0800

    redirect root to /docs

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Index from './views/Index.vue'
 import Home from './views/Home.vue'
 import Category from './views/Category.vue'
 
@@ -7,6 +8,10 @@ Vue.use(VueRouter)
 
 const baseUrl = '/docs'
 const routes = [{
+  path: `/`,
+  name: 'index',
+  component: Index
+}, {
   path: `${baseUrl}/`,
   name: 'home',
   component: Home
---
+++ src/views/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Redirecting..</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$router.push('/docs')
+  }
+}
+</script>
