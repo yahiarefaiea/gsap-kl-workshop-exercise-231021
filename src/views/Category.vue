7f26313154d591d933ec4597854daeddaccb7873
Yahia Refaiea <YahiaRefaiea@gmail.com>
2023-04-13 10:21:14 +0800

    add category

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,3 +1,74 @@
 <template>
-<div>Category</div>
+<div class="section">
+  <div class="container">
+    <tawk-breadcrumb
+      :items="breadcrumbItems"
+      :style="breadcrumbStyle"
+    />
+    <div>
+      <tawk-ui-card
+        v-if="!isEmpty(category)"
+        :item="category"
+        :type="expanded"
+      />
+    </div>
+  </div>
+</div>
 </template>
+
+<script>
+import {isEmpty} from 'lodash'
+import axios from 'axios'
+import Breadcrumb from '../components/Breadcrumb.vue'
+import UICard from '../components/UICard.vue'
+
+export default {
+  components: {
+    'tawk-breadcrumb': Breadcrumb,
+    'tawk-ui-card': UICard
+  },
+  data: () => ({
+    isEmpty,
+    breadcrumbItems: [],
+    category: {}
+  }),
+  computed: {
+    breadcrumbStyle: () => {
+      return {flex: 1, marginTop: `${21 - 60}px`, marginBottom: '22px'}
+    }
+  },
+  watch: {
+    category() {
+      this.breadcrumbItems = [...this.breadcrumbItems, {
+        title: this.category.title
+      }]
+    }
+  },
+  methods: {
+    extractCategoryById() {
+      const path = this.$route.path
+      // I'm sure there is a better way to extract the id below..
+      return path.split('-').reverse()[0]
+    },
+    async fetchCategoryById(id) {
+      axios.get('/api/categories')
+        .then(response => {
+          this.category = response.data.filter(item => item.id === id)[0]
+        })
+        .catch(error => ({error: JSON.stringify(error)}))
+    },
+  },
+  created() {
+    const id = this.extractCategoryById()
+    this.fetchCategoryById(id)
+    this.breadcrumbItems = [{
+      title: 'All categories',
+      path: '/'
+    }]
+  }
+}
+</script>
+<!--
+<style lang="scss" scoped>
+@import '../scss/_variables.scss';
+</style> -->
