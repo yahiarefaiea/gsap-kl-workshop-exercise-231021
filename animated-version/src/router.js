d79cba887d0f542eebe4ee0d8fb2ae22a0d98bbb
Yahia Refaiea <YahiaRefaiea@gmail.com>
2023-04-15 08:03:30 +0800

    remove other pages

--- animated-version/src/router.js
+++ animated-version/src/router.js
@@ -1,24 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Index from './views/Index.vue'
 import Home from './views/Home.vue'
-import Category from './views/Category.vue'
 
 Vue.use(VueRouter)
 
-const baseUrl = '/docs'
+const baseUrl = ''
 const routes = [{
-  path: `/`,
-  name: 'index',
-  component: Index
-}, {
   path: `${baseUrl}/`,
   name: 'home',
   component: Home
-}, {
-  path: `${baseUrl}/:id`,
-  name: 'category',
-  component: Category
 }]
 
 const router = new VueRouter({
