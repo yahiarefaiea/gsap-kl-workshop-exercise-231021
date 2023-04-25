fbbc89c8dde3981f269daaca21b83bba153ad3f3
Yahia Refaiea <yahiarefaiea@gmail.com>
2023-04-26 06:41:20 +0800

    add FlyingParrot component

---
+++ animated-version/src/components/FlyingParrot.vue
@@ -0,0 +1,20 @@
+<template>
+  <lottie ref="flyingParrot" :options="animationOptions" />
+</template>
+
+<script>
+import Lottie from 'vue-lottie'
+import animationData from '../assets/animations/flying-parrot.json'
+
+export default {
+  components: {Lottie},
+  data: () => ({
+    animationOptions: {
+      animationData,
+      renderer: 'html',
+      loop: true,
+      autoplay: true
+    }
+  })
+}
+</script>
