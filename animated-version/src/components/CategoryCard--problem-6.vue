a4cfe33fcd6c3d2e9d7c196ac9995871ea35dc89
Yahia Refaiea <yahiarefaiea@gmail.com>
2023-10-21 02:23:42 +0800

    add step-by-step animation process

---
+++ animated-version/src/components/CategoryCard--problem-6.vue
@@ -0,0 +1,141 @@
+<template>
+<a
+  class="category-card"
+  :href="`#${slug}`"
+  ref="categoryCard"
+  @click="handleCategoryClick"
+>
+  <div class="block">
+    <img :src="`/icons/${item.icon}.png`">
+
+    <h3 v-if="type === 'default'">{{item.title}}</h3>
+    <h2 v-else-if="type === 'expanded'">{{item.title}}</h2>
+
+    <span class="text-primary" v-if="type === 'default'">{{item.totalArticle}} articles</span>
+    <span class="text-gray">Last update {{formatDateFromNow(item.updatedOn)}}</span>
+  </div>
+  <div class="block" v-if="type === 'expanded'">
+    <p>{{item.description}}</p>
+  </div>
+</a>
+</template>
+
+<script>
+import {gsap} from 'gsap'
+import {formatDateFromNow} from '../utils'
+import slugify from 'slugify'
+
+export default {
+  props: {
+    item: {type: Object, required: true},
+    type: {type: String, default: 'default'}
+  },
+  data: () => ({
+    formatDateFromNow,
+    coordinates: {},
+    replacement: null,
+    timeline: null
+  }),
+  computed: {
+    slug() {
+      const {title, id} = this.item
+      return this.createSlug(title, id)
+    }
+  },
+  methods: {
+    createSlug: (title, id) => `${slugify(title, {lower: true})}-${id}`,
+    replaceCardWithPlaceholder(item) {
+      if(!this.replacement) {
+        this.replacement = document.createElement('div')
+        this.replacement.style.flex = `calc(${100/3}% - 20px)`
+        this.replacement.style.flexGrow = 0
+        this.replacement.style.visibility = 'hidden'
+        this.replacement.style.opacity = 0
+        item.insertAdjacentElement('afterend', this.replacement)
+      } else return
+    },
+    getCoordinates(item) {
+      this.coordinates.width = gsap.getProperty(item, 'offsetWidth')
+      this.coordinates.height = gsap.getProperty(item, 'offsetHeight')
+      this.coordinates.top = gsap.getProperty(item, 'offsetTop')
+      this.coordinates.left = gsap.getProperty(item, 'offsetLeft')
+    },
+    moveCard(item) {
+      const {width, height, top, left} = this.coordinates
+      gsap.set(item, {position: 'absolute', width, height, top, left})
+      this.timeline = gsap.timeline()
+      this.timeline.to(item, {
+        duration: 0.7,
+        ease: 'Power3.easeOut',
+        position: 'absolute',
+        top: 65,
+        left: 0,
+        transform: 'translate(-50%, -50%)'
+      })
+    },
+    handleCategoryClick() {
+      this.$emit('click', this.item)
+      const cardRef = this.$refs.categoryCard
+      this.replaceCardWithPlaceholder(cardRef)
+      this.getCoordinates(cardRef)
+      this.moveCard(cardRef)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/_variables.scss';
+
+.category-card {
+  display: block;
+  border: $category-card--border-size solid $category-card--border-color;
+  border-radius: $category-card--border-radius;
+  text-decoration: none;
+  background-color: $category-card--bg-normal-color;
+  > .block {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: $category-card--padding-y $category-card--padding-x;
+    &:not(:first-child) {
+      border-top: $category-card--border-size solid $category-card--border-color;
+    }
+  }
+  img {
+    margin-bottom: 25px;
+  }
+  h2, h3 {
+    text-align: center;
+  }
+  h3 {
+    margin-bottom: 21px;
+    font-weight: bold;
+  }
+  p {
+    font-size: 13px;
+    line-height: 18px;
+    color: $text-gray;
+  }
+}
+</style>
+
+<!-- global styles -->
+<style lang="scss">
+@import '../scss/_variables.scss';
+
+h2, h3, .text-primary, .text-gray {
+  font-family: $font-family;
+  color: $text-black;
+}
+.text-primary {
+  font-size: 13px;
+  line-height: 15.6px;
+  color: $green;
+}
+.text-gray {
+  font-size: 11px;
+  line-height: 13.2px;
+  color: $text-gray;
+}
+</style>
---
+++ animated-version/src/views/Home--problem-8.vue
@@ -0,0 +1,229 @@
+<template>
+<div class="section">
+  <div
+    class="container"
+    :style="containerStyle"
+  >
+    <tawk-breadcrumb :items="breadcrumbItems" :style="breadcrumbStyle" />
+
+    <div class="grid-view">
+      <div :style="categoryCardStyle" />
+
+      <div class="list-view">
+        <tawk-article-card
+          v-for="(article, index) in articles"
+          :key="`${kebabCase(article.title)}-${index}`"
+          :item="article"
+        />
+      </div>
+    </div>
+  </div>
+
+  <div class="container grid-view">
+    <p v-if="!queryFilteredItems" class="fallback">
+      Loading..
+    </p>
+    <p v-else-if="queryFilteredItems && queryFilteredItems.length === 0" class="fallback">
+      {{`No items were found matching “${this.searchQuery}”.`}}
+    </p>
+    <tawk-category-card
+      v-else-if="queryFilteredItems && queryFilteredItems.length > 0"
+      v-for="item in queryFilteredItems"
+      :key="item.id"
+      :item="item"
+      :style="categoryCardStyle"
+      ref="categoryCards"
+      @click="handleCategoryClick(item)"
+    />
+  </div>
+</div>
+</template>
+
+<script>
+import _, {isEmpty, kebabCase} from 'lodash'
+import axios from 'axios'
+import {gsap} from 'gsap'
+import Breadcrumb from '../components/Breadcrumb.vue'
+import CategoryCard from '../components/CategoryCard.vue'
+import ArticleCard from '../components/ArticleCard.vue'
+import {eventBus} from '../utils'
+
+export default {
+  components: {
+    'tawk-breadcrumb': Breadcrumb,
+    'tawk-category-card': CategoryCard,
+    'tawk-article-card': ArticleCard
+  },
+  data: () => ({
+    isEmpty,
+    kebabCase,
+    breadcrumbItems: [],
+    items: null,
+    queryFilteredItems: null,
+    searchQuery: '',
+    timeline: null,
+    category: {},
+    articles: []
+  }),
+  computed: {
+    breadcrumbStyle: () => {
+      return {flex: 1, marginTop: `${21 - 60}px`, marginBottom: '22px'}
+    },
+    categoryCardStyle: () => {
+      return {flex: `calc(${100/3}% - 20px)`, flexGrow: 0}
+    },
+    containerStyle: () => {
+      return {
+        position: 'absolute',
+        top: 0,
+        left: '50%',
+        width: '985px',
+        marginLeft: `${-985/2}px`
+      }
+    }
+  },
+  watch: {
+    searchQuery() {
+      this.filterItemsBasedOnQuery()
+    },
+    category() {
+      this.breadcrumbItems = [this.breadcrumbItems[0], {
+        title: this.category.title
+      }]
+    },
+    '$route.hash': function() {
+      const id = this.extractCategoryId()
+      if(!!id) {
+        this.fetchCategoryById(id)
+        this.fetchArticles()
+      }
+    }
+  },
+  methods: {
+    isEnabled: item => item.enabled,
+    orderItems: items => items.sort((a, b) => a.order - b.order),
+    filterItems(items) {
+      return items.filter(item => this.isEnabled(item))
+    },
+    filterItemsBasedOnQuery() {
+      this.queryFilteredItems = this.items.filter(item => _.includes(
+        // converting strings below to lower case for the seek of a complete demo, nothing more..
+        _.toLower(item.title),
+        _.toLower(this.searchQuery)
+      ))
+    },
+    async fetchCategories() {
+      axios.get('/api/categories')
+        .then(response => {
+          this.items = this.filterItems(response.data)
+          this.items = this.orderItems(this.items)
+          this.queryFilteredItems = this.items
+        })
+        .catch(error => ({error: JSON.stringify(error)}))
+    },
+    cleanUrl() {
+      const {path, hash} = this.$route
+      if(hash) this.$router.push(path)
+    },
+    extractCategoryId() {
+      const {hash} = this.$route
+      // I'm sure there is a better way to extract the id below..
+      return hash.split('-').reverse()[0]
+    },
+    async fetchCategoryById(id) {
+      this.category = this.queryFilteredItems.filter(item => item.id === id)[0]
+    },
+    async fetchArticles() {
+      axios.get('/api/category/')
+        .then(response => {
+          this.articles = response.data.filter(item => item.status === 'published')
+        })
+        .catch(error => ({error: JSON.stringify(error)}))
+    },
+    onQueryChanged(val) {
+      this.searchQuery = val
+    },
+    handleCategoryClick(item) {
+      const siblingsRefs = this.$refs.categoryCards.filter(card => card._props.item.id !== item.id)
+      const siblings = siblingsRefs.map(siblingCard => siblingCard.$el)
+      this.hideSiblings(siblings)
+    },
+    hideSiblings(siblings) {
+      this.timeline = gsap.timeline({
+        onComplete: () => {
+          gsap.set(siblings, {visibility: 'hidden'})
+        }
+      })
+      this.timeline.staggerTo(siblings, 0.5, {
+        ease: 'Power3.easeOut',
+        opacity: 0
+      }, 0.2)
+    }
+  },
+  created() {
+    this.cleanUrl()
+    this.fetchCategories()
+    eventBus.$on('filter:items', this.onQueryChanged)
+
+    this.breadcrumbItems = [{
+      title: 'All categories',
+      path: '/'
+    }]
+  },
+  destroyed() {
+    eventBus.$off('filter:items', this.onQueryChanged)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/_variables.scss';
+
+.fallback {
+  flex: 1;
+  color: $text-gray;
+}
+
+.section .container .grid-view {
+  transform: translateX(0);
+}
+</style>
+
+<!-- global styles -->
+<style lang="scss">
+@import '../scss/_variables.scss';
+
+p {
+  font-family: $font-family;
+  text-align: center;
+  color: $text-black;
+}
+
+.section {
+  position: relative;
+  background-color: $light-gray;
+  .container {
+    position: relative;
+    max-width: $container--width;
+    margin: 0 auto;
+    padding: $container--padding-y 0;
+  }
+  .grid-view {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    gap: $container-grid--gap;
+    position: relative;
+    transform: translateX($container-grid--gap*0.5);
+  }
+  .grid-view, .container.grid-view {
+    max-width: $container-grid--width;
+  }
+  .list-view {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    gap: $container-grid--gap;
+  }
+}
+</style>
